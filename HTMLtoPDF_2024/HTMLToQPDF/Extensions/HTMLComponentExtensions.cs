4b1794a98418c569411f675b77c47c3b82054a45
Serverraum <fa.eschenbruecher@gmail.com>
2024-05-13 23:30:27 +0200

    Update

---
+++ HTMLtoPDF_2024/HTMLToQPDF/Extensions/HTMLComponentExtensions.cs
@@ -0,0 +1,15 @@
+﻿using QuestPDF.Fluent;
+using QuestPDF.Infrastructure;
+
+namespace HTMLQuestPDF.Extensions
+{
+    public static class HTMLComponentExtensions
+    {
+        public static void HTML(this IContainer container, Action<HTMLDescriptor> handler)
+        {
+            var htmlPageDescriptor = new HTMLDescriptor();
+            handler(htmlPageDescriptor);
+            container.Component(htmlPageDescriptor.PDFPage);
+        }
+    }
+}
